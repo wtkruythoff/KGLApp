95583b10b0b82c472f71b1f8cc4f53828bed2d14
wtkruythoff <wtkruythoff@outlook.com>
2021-01-06 23:04:25 +0100

    changes diverse

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { useState } from 'react';
-import ReactMapGL from "react-map-gl";
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import * as locationData from "./data/SouligaRoad01.json";
 
 
 
@@ -13,13 +14,54 @@ export default function App() {
     zoom: 10
   });
 
+  const [selectedPark, setSelectedPark] = useState(null);
+
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+      window.addEventListener("keydown", listener);
+    
+      return () => {
+        window.removeEventListener("keydown", listener);
+      }
+  }, []);
+
   return (
     <div>
     <ReactMapGL
     {...viewport}
     mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+    mapStyle="mapbox://styles/wtkruythoff/ckjiy3q3i03ux19stagf5oyj0"
     onViewportChange={(viewport) => setViewport(viewport)}
-    >points</ReactMapGL>
+    >
+    {locationData.features.map(location => (
+      <Marker key={location.properties.BuildCode} latitude={location.geometry.coordinates[1]} longitude={location.geometry.coordinates[0]} >
+      <button className="marker-btn" onClick={e => {
+        e.preventDefault();
+        setSelectedPark(location);
+      }}>
+      <img src="/map-marker-alt-light.svg" alt="Location Icon"/>
+      </button>
+      </Marker>
+    ))}
+    {selectedPark ? (
+      <Popup
+      latitude={selectedPark.geometry.coordinates[1]}
+      longitude={selectedPark.geometry.coordinates[0]}
+      onClose={() => {
+        setSelectedPark(null);
+      }}
+      >
+      <div>
+      <h2>{selectedPark.properties.StreetName}</h2>
+      <p>{selectedPark.properties.StreetNumb}</p>
+      </div>
+      </Popup>
+    ) : null}
+    </ReactMapGL>
     </div>
   );
 }
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Mapbox - Interactive maps in React
+https://www.youtube.com/watch?v=JJatzkPcmoI&list=RDCMUCWPY8W-FAZ2HdDiJp2RC_sQ&start_radio=1&ab_channel=LeighHalliday
+
+https://visgl.github.io/react-map-gl/
